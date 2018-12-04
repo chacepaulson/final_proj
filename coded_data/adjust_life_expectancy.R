f040b81eebee0d487c2315d219b9bb90b11726e5
chacepaulson <paulson.chace@gmail.com>
2018-12-04 11:01:28 -0500

    add life expectancy

---
+++ coded_data/adjust_life_expectancy.R
@@ -0,0 +1,47 @@
+# import life expectancy rates data set 
+library(readxl)
+life_expectancy <- read_excel("~/Downloads/final_proj/raw_data/life_expectancy.xlsx")
+
+library(tidyverse)
+life_expectancy <- as_tibble(life_expectancy)
+
+# rename columns
+library(magrittr)
+colnames(life_expectancy)
+life_expectancy <- life_expectancy %>% 
+  rename(country_name = `Country Name`, 
+         life_expectancy = `Life Expectancy`)
+colnames(life_expectancy)
+
+# split the string in column 1 to put the country_name in the second column
+life2 <- NA
+life2 <- str_split_fixed(life_expectancy$country_name, " ", n = 2)
+
+# convert to tibble and rename columns
+life2 <- as.tibble(life2)
+life2 <- life2 %>% 
+  rename(country_id = `V1`, 
+         country_name = `V2`) 
+life2$life_expectancy <- NA
+
+# trim the leading white spaces off of the country_name column
+head(life2)
+life2$country_name <- trimws(life2$country_name)
+
+# put the life_expectancy rates into third column
+library(dplyr)
+life2 <- life2 %>% 
+  separate(country_name, into = c("country_name", "life_expectancy"), 
+           sep = "(?<=[a-zA-Z])\\s*(?=[0-9])")
+head(life2)
+
+# merge with sovereign nations to keep only desired nations
+life_mixed <- merge(sovereign_nations, life2, all.x = TRUE)
+
+# test for NAs 
+sum(is.na(life_mixed$life_expectancy))
+life_mixed$country_name[is.na(life_mixed$life_expectancy)]
+
+# create smaller tibble keeping only country_name and life_expectancy 
+small_life <- data.frame("country_name" = life_mixed$country_name, 
+                           "life_expectancy" = life_mixed$life_expectancy)
--- coded_data/success_data.R
+++ coded_data/success_data.R
@@ -24,8 +24,11 @@ success_data <- merge(success_data, small_unemployment, all.x = TRUE)
 # merge in the literacy data 
 success_data <- merge(success_data, small_literacy, all.x = TRUE)
 
-# merge in the literacy data 
+# merge in the median income data 
 success_data <- merge(success_data, small_median_income, all.x = TRUE)
+
+# merge in the life expectancy data 
+sucess_data <- merge(success_data, small_life, all.x = TRUE)
 
 # merge in the infant mortality data 
 sucess_data <- merge(success_data, small_infant, all.x = TRUE)
